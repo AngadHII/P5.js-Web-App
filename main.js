d0985e63d56cc997767d8aad7875277ab361b258
AngadHII <130420814+AngadHII@users.noreply.github.com>
2023-04-11 13:36:20 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup(){
+Canvas = createCanvas(500,500);
+Canvas.center();
+}
+
+function draw(){
+    fill(21,239,28);
+    circle(400, 110, 80);
+    fill(255,0,0,63);
+    ellipse(56, 46, 80, 40);
+    fill(18,229,245,127);
+    rect(90, 80, 150, 280, 20);
+}
